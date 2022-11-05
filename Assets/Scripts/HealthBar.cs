80053643d6ccb6ca09b52e5bd68c17530ad3a64d
LikeMen22 <romanekjigl@seznam.cz>
2022-11-05 20:25:49 +0100

    Adjusting Scripts

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TowerFollowTarget : MonoBehaviour
+public class HealthBar : MonoBehaviour
 {
-    
+    // Start is called before the first frame update
     void Start()
     {
         
@@ -14,10 +14,5 @@ public class TowerFollowTarget : MonoBehaviour
     void Update()
     {
         
-        {
-            
-        }
-
-
-    }   
+    }
 }
